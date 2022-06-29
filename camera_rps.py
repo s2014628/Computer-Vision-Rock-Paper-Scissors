590e1f37cd42c163e73c458456099fa24ca3454a
Tao Feng <71760715+s2014628@users.noreply.github.com>
2022-06-30 00:32:12 +0100

    final version

--- camera_rps.py
+++ camera_rps.py
@@ -1,16 +1,12 @@
-def get_user_choice():
-    '''
-    This is the 
-    '''
-    
-    user_choice = input("what is you choice? Please enter r for (rock), p for (papper) or s for (scissors)")
-
-    if user_choice == "r":
-        return "rock"
-    elif user_choice == "p":
-        return "paper"
-    elif user_choice =="s":
-        return "scissors"
+def get_prediction(prediction):
+    if prediction[0][0]>0.5:
+        return'Rock'
+    elif prediction[0][1]>0.5:
+        return'Papper'
+    elif prediction[0][2]>0.5:
+        return'Scissors'
+    elif prediction[0][3]>0.5:
+        return'Nothing'
 
 def get_computer_choice(choice_list):
     import random   
@@ -35,12 +31,3 @@ def who_won(user_choice, computer_choice):
             return " computer wins"
         elif computer_choice == 'rock':
             return " you win"
-def play():
-    choice_list = ['rock', 'paper', 'scissors']
-    computer_choice = get_computer_choice(choice_list)
-    user_choice = get_user_choice()
-
-    print("Your choice is", user_choice)
-    print("The computer choice is", computer_choice)
-    print(who_won(user_choice, computer_choice))
-play()
